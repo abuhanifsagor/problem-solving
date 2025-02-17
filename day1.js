cc1e57347b279fbf84cbd418f1ba194e95fd3274
abuhanifsagor <abuhanifsagor72@gmail.com>
2025-02-17 14:33:40 +0600

    done countwords

--- day1.js
+++ day1.js
@@ -1,14 +1,10 @@
-
-function printNumber (N){
-
-  
-        if(N >= 0){
-            for(let i = 0; i <= N; i++)
-                {
-                   console.log(i);
-          
-                }
-        }
+function printNumber(N) {
+  if (N >= 0) {
+    for (let i = 0; i <= N; i++) {
+      console.log(i);
+    }
+  }
 }
 
 printNumber(5);
+//output :0 1 2 3 4 5
