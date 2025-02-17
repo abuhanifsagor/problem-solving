b34b0927ecb2b23ccb231ac9dfcb81c4c9ffd896
abuhanifsagor <abuhanifsagor72@gmail.com>
2025-02-17 12:10:40 +0600

    day 1 done

---
+++ day1.js
@@ -0,0 +1,14 @@
+
+function printNumber (N){
+
+  
+        if(N >= 0){
+            for(let i = 0; i <= N; i++)
+                {
+                   console.log(i);
+          
+                }
+        }
+}
+
+printNumber(5);
