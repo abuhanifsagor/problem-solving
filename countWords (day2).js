1b5a00ba5718b91e9797e6525497bf50d03100d9
abuhanifsagor <abuhanifsagor72@gmail.com>
2025-02-17 14:03:45 +0600

    day 2 (countWord) done

---
+++ countWords (day2).js
@@ -0,0 +1,22 @@
+function countWords(sentence) {
+  let words = 0;
+  let isWord = false;
+
+  for (const char of sentence) {
+   
+    if (char != " ") {
+      if (!isWord) {
+        words++;
+        isWord = true;
+      }
+    } else {
+      isWord = false;
+    }
+  }
+
+  return words;
+}
+
+const output = countWords("DEV ZeroOne is a Next-Gen Programming Learning Platform.");
+const output2 = countWords("       ");
+console.log(output,output2);
